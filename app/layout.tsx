907ffccb2e1dadf491da49069cf976ebdddb1dcc
Israel Lissak <islissak@gmail.com>
2025-04-18 17:23:59 +0300

    Add application logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import { ThemeProvider } from './context/ThemeContext'
 export const metadata = {
   title: 'ספירת העומר',
   description: 'אפליקציה לספירת העומר',
+  icons: {
+    icon: '/omer-logo-he.png',
+    apple: '/omer-logo-he.png',
+  },
 }
 
 export default function RootLayout({
